da274c399bf81d05ba2ba13cbf5e90962903744a
krz <maxlav78@gmail.com>
2024-03-18 23:25:59 +0100

    Updated release schema

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,5 +2,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import track from './track.jsx'
+import release from './release'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, release, track, blockContent]
