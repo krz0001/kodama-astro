1b4bc0e672b21f8a9866d7f31a7c91cc3da4a7bd
krz <maxlav78@gmail.com>
2024-03-23 21:44:36 +0100

    Add i18n support and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
+import liciousI18n from "@astrolicious/i18n";
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET;
 
@@ -23,7 +24,14 @@ export default defineConfig({
     // `false` if you want to ensure fresh data
     apiVersion: "2024-03-18" // Set to date of setup to use the latest API version
   }), react() // Required for Sanity Studio
-  , tailwind()],
+    , tailwind(), liciousI18n({
+      defaultLocale: "en",
+      locales: ["en", "jp"],
+      strategy: "prefix",
+      client: {
+        data: true,
+      }
+    })],
   redirects: {
     "/admin/[...path]": "/admin"
   },
