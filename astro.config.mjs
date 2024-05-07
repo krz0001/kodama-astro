c7dba7460fa60f48aaf7d4add9738f9cd2b5b3d7
krz <maxlav78@gmail.com>
2024-05-07 19:23:03 +0200

    Refactor discography component, update Astro configuration, and add new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import liciousI18n from "@astrolicious/i18n";
+import icon from "astro-icon";
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET;
 
@@ -24,14 +25,14 @@ export default defineConfig({
     // `false` if you want to ensure fresh data
     apiVersion: "2024-03-18" // Set to date of setup to use the latest API version
   }), react() // Required for Sanity Studio
-    , tailwind(), liciousI18n({
-      defaultLocale: "en",
-      locales: ["en", "jp"],
-      strategy: "prefixExceptDefault",
-      client: {
-        data: true,
-      }
-    })],
+  , tailwind(), liciousI18n({
+    defaultLocale: "en",
+    locales: ["en", "jp"],
+    strategy: "prefixExceptDefault",
+    client: {
+      data: true
+    }
+  }), icon()],
   redirects: {
     "/admin/[...path]": "/admin"
   },
