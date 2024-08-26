3f47591297a58b49fc6d2ce043ec795c17ed05bc
krz <maxlav78@gmail.com>
2024-08-26 23:19:06 +0200

    feat: Update post schema to use internationalizedArrayBlockContent for body

--- schema/post.js
+++ schema/post.js
@@ -33,7 +33,7 @@ export default defineType({
     defineField({
       name: 'body',
       title: 'Body',
-      type: 'blockContent',
+      type: 'internationalizedArrayBlockContent',
     }),
   ],
 
