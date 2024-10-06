6fb7efbd88a0f5f763e267e2b02a5e2fb6ae6e7d
krz <maxlav78@gmail.com>
2024-10-06 21:02:41 +0200

    feat: Add project schema to index.js
    This commit adds the project schema to the index.js file in the schema directory. The project schema is imported and included in the 'schema' array along with other schemas like post, release, blockContent, collaborator, staff, and calloutButton.

--- schema/index.js
+++ schema/index.js
@@ -3,7 +3,8 @@ import post from './post.js'
 import release from './release.jsx'
 import collaborator from './collaborator.jsx'
 import staff from './staff.jsx'
+import project from './project.js'
 
 import calloutButton from './components/calloutButton.jsx'
 
-export const schema = [post, release, blockContent, collaborator, staff, calloutButton]
+export const schema = [post, release, blockContent, collaborator, staff, calloutButton, project]
--- src/utils/sanity.js
+++ src/utils/sanity.js
@@ -1,6 +1,7 @@
 //import { useSanityClient } from "@sanity/astro";
 import {createClient} from '@sanity/client'
 import groq from "groq";
+import { toHTML } from "@portabletext/to-html";
 
 const projectId = import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID;
 const dataset = import.meta.env.PUBLIC_SANITY_STUDIO_DATASET;
@@ -13,6 +14,7 @@ const client = createClient(
     }
 )
 
+//#region Posts
 export async function getPosts() {
     return await client.fetch(groq `*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`);
 }
@@ -21,7 +23,20 @@ export async function getPost(slug) {
         slug,
     });
 }
+//#endregion
 
+//#region Projects
+export async function getProjects() {
+    return await client.fetch(groq `*[_type == "project" && defined(slug.current)] | order(_createdAt desc)`);
+}
+export async function getProject(slug) {
+    return await client.fetch(groq `*[_type == "project" && slug.current == $slug][0]`, {
+        slug,
+    });
+}
+//#endregion
+
+//#region Releases
 export async function getReleases() {
     return await client.fetch(groq `*[_type == "release" && defined(slug.current)] | order(_createdAt desc)`);
 }
@@ -35,7 +50,9 @@ export async function getRelease(slug) {
         slug,
     });
 }
+//#endregion
 
+//#region Staff
 export async function getStaff() {
     return await client.fetch(groq `*[_type == "staff"] | order(_createdAt desc)`);
 }
@@ -44,9 +61,49 @@ export async function getStaffMember(slug) {
         slug,
     });
 }
+//#endregion
 
 export async function getRef(ref) {
     return await client.fetch(groq `*[_id == $ref][0]`, {
         ref,
     });
+}
+
+
+//#region Portable Text Components
+export function toCalloutButton(value) {
+  console.log(value);
+  // get the URL and the type of content
+  let url = "/projects/rules";
+  let content = value.content;
+  if (content === "projectSubmission") {
+    url = value.googleFormUrl;
+  }
+
+  // TODO: add icon, and localize the button string
+  let buttonString, iconName;
+  switch (value.content) {
+    case "projectSubmission":
+      // envelope icon
+      iconName = "envelope";
+      buttonString = "Please fill in the Google Forms";
+      break;
+    case "rules":
+      // book icon
+      iconName = "book";
+      buttonString = "You can check the rules & submission patterns common to all releases here.";
+      break;
+    default:
+      // ifnformation icon
+      iconName = "info";
+      buttonString = "Learn more";
+  }
+
+  // yeah uhhh just reference the icon svg in the string yeah bro
+  return `<a href="${url}" class="kodama_btn kodama_btn--primary kodama_btn--lg mx-auto my-2 no-underline text-xl">
+    <svg width="1em" height="1em" viewBox="0 0 512 512" data-icon="${iconName}">
+      <use xlink:href="#ai:local:${iconName}" />
+    </svg>
+    ${buttonString}
+  </a>`;
 }
